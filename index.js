7763353f6c69a60516017a2c4f77d2ef97cdc992
linharesrocha <guilhermelinharesr@gmail.com>
2021-12-18 19:28:12 -0300

    capturando dados de um formulario

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(express.urlencoded ({extended: true}));
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.render("index");
 })
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulario recebido " + titulo + " " + " descricao " + descricao)
 })
 
 app.listen(8080, (error) => {
