69a39b7ae90f5f838c38be0fbda5acee1aac7213
linharesrocha <guilhermelinharesr@gmail.com>
2021-12-18 20:54:53 -0300

    busca condicional com sequelize

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ app.post("/salvarpergunta", (req, res) => {
     }).then(() => {
         res.redirect("/");
     });
+});
+
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined) {
+            res.render("pergunta");
+        }else {
+            res.redirect("/");
+        }
+    });
 })
 
 app.listen(8080, (error) => {
