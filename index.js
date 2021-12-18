88b9365deb63fc7a55f1f31a851d9c43ae0b739a
linharesrocha <guilhermelinharesr@gmail.com>
2021-12-18 20:22:25 -0300

    gerando tabela com model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const connection = require("./database/database")
+const connection = require("./database/database");
+const perguntaModel = require("./database/Pergunta");
 
 connection.authenticate().then(() => {
     console.log("Conexão feita com o banco de dados!")
