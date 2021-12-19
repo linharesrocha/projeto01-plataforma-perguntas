20341bca5e4e7fabc7ab0c003d05a4251f103c18
linharesrocha <guilhermelinharesr@gmail.com>
2021-12-18 21:49:56 -0300

    respondendo perguntas

--- index.js
+++ index.js
@@ -56,7 +56,18 @@ app.get("/pergunta/:id", (req, res) => {
             res.redirect("/");
         }
     });
-})
+});
+
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId);
+    });
+});
 
 app.listen(8080, (error) => {
     error == true ? console.log("Deu ruim") : console.log("Servidor rodando!")
